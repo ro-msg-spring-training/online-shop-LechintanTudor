986f23815e8ec2f22fe7740fabee2a4916773311
Tudor Lechintan <lechintantudor02@gmail.com>
2022-07-29 11:26:57 +0300

    Exclude username and password from CustomerDto

--- shop/src/main/java/ro/msg/learning/shop/dto/CustomerDto.java
+++ shop/src/main/java/ro/msg/learning/shop/dto/CustomerDto.java
@@ -8,16 +8,12 @@ public class CustomerDto {
     private final Long id;
     private final String firstName;
     private final String lastName;
-    private final String username;
-    private final String password;
     private final String email;
 
     public CustomerDto(Customer customer) {
         id = customer.getId();
         firstName = customer.getFirstName();
         lastName = customer.getLastName();
-        username = customer.getUsername();
-        password = customer.getPassword();
         email = customer.getEmail();
     }
 }
